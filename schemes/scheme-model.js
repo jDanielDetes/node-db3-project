298e0ac95740961c6734f49545a29ae9de0d5dca
jDanielDetes <jdanieldetes@gmail.com>
2020-02-20 16:34:37 -0500

    finished mvp

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,4 +1,4 @@
-const db = require('../data/db-config')
+const db = require('./dbConfig.js')
 
 
 module.exports ={
@@ -10,3 +10,41 @@ module.exports ={
     update,
     remove
 }
+
+
+function find(){
+    return db("schemes")
+
+}
+
+function findById(id){
+    return db("schemes")
+    .where({id})
+    .first()
+}
+
+function findSteps(id) {
+    return db('steps')
+    .join("schemes", "schemes.id", "=", "steps.scheme_id")
+}
+
+
+function add(scheme){
+    return db('schemes').insert(scheme, 'id').then(ids => findById(ids[0]))
+
+}
+
+
+function update(changes, id) {
+    return db('schemes')
+    .update(changes)
+    .where({id})
+  }
+  
+
+
+  function remove(id) {
+    return db('schemes')
+    .where({id})
+    .delete()
+  }
